800bf3cae6a55f51df16d2a2d2cbde553dbe5aee
Shedrick Williams <lilshed3@gmail.com>
2026-01-24 23:11:32 -0800

    unsaved change in ui/mod.rs

--- src/util/io/ui/mod.rs
+++ src/util/io/ui/mod.rs
@@ -2,22 +2,6 @@
  * UI/Style Definitions, Component architecture
  *****************************************/
 
-/****************************************************
- * 'Bits' are the highest level UI component
- *      'Bits' are comprised of 'Pieces',
- *      which are the lowest level UI Components
- *
- *      For now, styles can only be applied to Pieces
- ****************************************************/
-
-/*
-UPDATE:
-
-'bits' will be renamed to App (singular), and
-'pieces' will be renamed to Components.
-
-See bits/mod.rs for more info.
-*/
 pub mod app;
 pub mod layout;
 
